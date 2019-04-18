b6ebded055501059b0c05238b2e69255b2e994e5
Kai Shoya <s-kai@lumber-mill.co.jp>
2019-04-18 14:36:20 +0900

    indexで価格一覧を表示するようにしました

--- pub/parts/navbar.php
+++ pub/parts/navbar.php
@@ -1,7 +1,6 @@
 <nav class="navbar is-dark">
 	<div id="navbarExampleTransparentExample" class="navbar-brand">
 		<div class="navbar-item">ホテル検索</div>
-		<!-- <a class="navbar-item <?= ($_SESSION["title"] == INDEX) ? "is-active" : "" ; ?>" href="<?= PUBLIC_TOP.'/index.php' ?>"><?= INDEX ?></a> -->
 		<a class="navbar-item <?= ($_SESSION["title"] == HOTEL_LIST) ? "is-active" : "" ; ?>" href="<?= PUBLIC_TOP.'/hotel_list.php' ?>"><?= HOTEL_LIST ?></a>
 		<a class="navbar-item <?= ($_SESSION["title"] == PRICE_LIST) ? "is-active" : "" ; ?>" href="<?= PUBLIC_TOP.'/price_list.php' ?>"><?= PRICE_LIST ?></a>
 	</div>
