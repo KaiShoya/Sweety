0a3d7a888886968900d3a690fe1821f24f6272c4
Kai Shoya <s-kai@lumber-mill.co.jp>
2019-04-26 13:22:12 +0900

    google analyticsを登録

--- pub/parts/head.php
+++ pub/parts/head.php
@@ -4,6 +4,15 @@ $file = pathinfo($associative_array[0]["file"])['filename'];
 ?>
 
 <head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-132761789-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-132761789-1');
+  </script>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title><?= $_SESSION["title"] ?></title>
