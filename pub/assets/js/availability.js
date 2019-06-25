af09fde5d6d2372321998939bc2d11ecd87f967b
Kai Shoya <s-kai@lumber-mill.co.jp>
2019-06-25 21:14:32 +0900

    空室数の入力欄を削除

--- pub/assets/js/availability.js
+++ pub/assets/js/availability.js
@@ -1,14 +1,10 @@
 var app = new Vue({
   el: ".container",
   data: {
-    available: 0,
-    vacancies: 0
+    available: 0
   },
   watch: {
     available: function (val) {
-      console.log(val)
-    },
-    vacancies: function (val) {
       console.log(val)
     }
   }
