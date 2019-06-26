59e88be8d3b04df61315ada2d4eda1afee94d434
Kai Shoya <s-kai@lumber-mill.co.jp>
2019-06-26 23:21:22 +0900

    プロダクション環境用設定ファイルにタイトル追加

--- sys/core/constant.php
+++ sys/core/constant.php
@@ -11,6 +11,10 @@ define("INDEX", 'トップ');
 define("HOTEL_LIST", 'ホテル一覧');
 define("PRICE_LIST", '価格一覧');
 define("TOS", '利用規約');
+define("AVAILABILITY", "空室状況");
+define("PRICE_CHANGE", "料金変更");
+define("LOGIN", "ログイン");
+define("LOGOUT", "ログアウト");
 
 // DB情報
 define("DB_TYPE", 'mysql'); // mysql or pgsql.
